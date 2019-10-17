cf5f8730a482dc957438cd23252c113bfbf9b201
danielforeverlearning <danielforeverlearning@gmail.com>
2019-10-16 14:58:12 -1000

    Add files via upload

---
+++ java_console_app/RestAPIResponse.java
@@ -0,0 +1,13 @@
+package console_restapi;
+
+public class RestAPIResponse {
+
+	public int responseCode;
+	public String response;
+	
+	public RestAPIResponse() {
+		responseCode = 0;
+		response = null;
+	}
+	
+}
